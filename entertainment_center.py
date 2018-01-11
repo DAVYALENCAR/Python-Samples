2995567aba8007434827b668f9977d29b1a1d0a8
DAVYALENCAR <35174458+DAVYALENCAR@users.noreply.github.com>
2018-01-11 10:44:29 -0300

    Add files via upload

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,19 +3,27 @@ import media
 
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
-                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "http://upload.wikimedia.org/" +
+                        "wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=-9oq-uVwDk8")
 
 wonder_woman = media.Movie("Wonder Woman",
-                           "Is an movie from 2017, based on the homonym character of DC Comics and distributed by Warner Bros. Pictures",
-                           "http://br.web.img2.acsta.net/r_1920_1080/pictures/16/11/03/16/33/533535.jpg",
+                           "Is an movie from 2017, based on the " +
+                           "homonym character of DC Comics and " +
+                           "distributed by Warner Bros. Pictures",
+                           "http://br.web.img2.acsta.net/r_1920_1080/" +
+                           "pictures/16/11/03/16/33/533535.jpg",
                            "https://www.youtube.com/watch?v=bldAkEUANWA")
 
 kingsman = media.Movie("Kingsman",
-                       "A spy organization recruits an unrefined, but promising street kid into the agency's ultra-competitive training program, just as a global threat emerges from a twisted tech genius",
-                       "http://t0.gstatic.com/images?q=tbn:ANd9GcRjbufWaA7TRXS4z_088Y6PKiIcLsV2wro_fTsQ39PuaI-OQnNf",
+                       "A spy organization recruits an unrefined, " +
+                       "but promising street kid into the agency's " +
+                       "ultra-competitive training program, " +
+                       "just as a globalthreat emerges from " +
+                       "a twisted tech genius",
+                       "http://t0.gstatic.com/images?q=tbn:ANd9GcRjbu" +
+                       "fWaA7TRXS4z_088Y6PKiIcLsV2wro_fTsQ39PuaI-OQnNf",
                        "https://youtu.be/LqehHzvyaKw")
 
-movies = [toy_story,wonder_woman,kingsman]
+movies = [toy_story, wonder_woman, kingsman]
 fresh_tomatoes.open_movies_page(movies)
-
